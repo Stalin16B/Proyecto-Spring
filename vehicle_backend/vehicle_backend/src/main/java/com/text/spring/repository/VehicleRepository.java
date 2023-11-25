e0255a70724e679aa128c6b6ba41b1eedcb86914
Stalin Muela <64552499+Stalin16B@users.noreply.github.com>
2023-11-25 16:08:17 -0500

    Delete vehicle_backend directory

--- vehicle_backend/vehicle_backend/src/main/java/com/text/spring/repository/VehicleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.text.spring.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.text.spring.model.VehicleModel;
-
-public interface VehicleRepository extends JpaRepository<VehicleModel,Long> {
-
-}
